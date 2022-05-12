05f7a10bc57f35e9e46063932f7d0c3cf3b2fa18
ihyperbr <ihyperbr@gmail.com>
2022-05-11 22:28:43 -0300

    Acabou por hoje

--- src/Rotas.js
+++ src/Rotas.js
@@ -1,6 +1,8 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { Login } from "./Components/Login/Index";
 import { Header } from "./Components/HeaderUsuario/HeaderUser";
+import { FooterGeral } from "./Components/FooterGeral/footer";
+
 function Rotas() {
   return (
     <>
@@ -9,6 +11,7 @@ function Rotas() {
           <Route path="/" element={<h1>Oi</h1>} />
           <Route path="/Login" element={<Login />} />
           <Route path="HeaderUser" element={<Header />} />
+          <Route path="/Footer" element={<FooterGeral />} />
         </Routes>
       </BrowserRouter>
     </>
