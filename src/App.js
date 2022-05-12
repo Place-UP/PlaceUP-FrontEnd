36d9f2c808178f71d978f3a92b7b9b2b83253c2f
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2022-05-11 21:31:58 -0300

    commit da noite

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import Rotas from "./routes.js";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
+
 export default function App() {
   return (
     <BrowserRouter>
       <Rotas />
-    </BrowserRouter>);
+    </BrowserRouter>
+  );
 }
-
 
