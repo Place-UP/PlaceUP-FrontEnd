b375d5d0a72a2becbdbfd68d2f4e49d9290b30d1
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2022-05-16 19:12:30 -0300

    commit com a indexcalculadora

--- src/Rotas.js
+++ src/Rotas.js
@@ -1,9 +1,6 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { Login } from "./Components/Login/Index";
-import { CadastroUser } from './Pages/cadastroUsuario'
-import {Header} from "./Components/HeaderUsuario/HeaderUser";
-import { HeaderComerciante} from "./Components/HeaderComerciantes/HeaderComerciante";
-import { FooterGeral } from "./Components/FooterGeral/footer";
+import { CadastroUser } from "./Pages/cadastroUsuario";
 import { IndexCalculadora } from "./Pages/PageCalculadora/index";
 import { Pagina404 } from "./Components/Error404";
 
@@ -15,15 +12,11 @@ function Rotas() {
           <Route path="/" element={<h1>Oi</h1>} />
           <Route path="/Login" element={<Login />} />
           <Route path="/CadastroUser" element={<CadastroUser />} />
-          <Route path="/HeaderUser" element={<Header />} />
-          <Route path="/HeaderComerciante" element={<HeaderComerciante />} />
-          <Route path="/Footer" element={<FooterGeral />} />
           <Route
             path="/Como-Funciona-Calculadora"
             element={<IndexCalculadora />}
           />
           <Route path="*" element={<Pagina404 />} />
-       
         </Routes>
       </BrowserRouter>
     </>
