5dd69fa08677147146a90b8866edf6caa9340dcf
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2022-05-18 22:02:34 -0300

    commit da noite explicando calculadora

--- src/Pages/PageCalculadora/styles.js
+++ src/Pages/PageCalculadora/styles.js
@@ -1,24 +1,85 @@
 import styled from "styled-components";
 
+const Media = {
+  Desktop: "@media(max-width:1024px)",
+  MobileL: "@media(max-width:425px)",
+  MobileM: "@media(max-width:375px)",
+  MobileS: "@media(max-width:35px)",
+};
 export const Main = styled.main`
   height: 100vh;
-  margin-left: 70px;
+  margin-left: 120px;
 `;
 
-export const BannerContainer = styled.div`
+export const Container = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
-
-  .texto {
-    font-size: 50px;
+  .text {
     font-family: Bakbak One;
+    font-size: 50px;
     color: #2d3e50;
-    margin-left: 30px;
+
+    ${Media.Desktop} {
+      font-size: 30px;
+    }
+    ${Media.MobileL} {
+      font-size: 20px;
+    }
   }
 
-  .imgs {
+  .vectorCalculator {
     max-width: 600px;
+    ${Media.Desktop} {
+      max-width: 300px;
+    }
+    ${Media.MobileL} {
+      width: 200px;
+    }
+  }
+`;
+
+export const HowToUseCalculator = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-right: 0;
+  img {
+    max-width: 600px;
+
+    ${Media.Desktop} {
+      max-width: 200px;
+    }
+  }
+  h1 {
+    font-size: 40px;
+    width: 80%;
+    color: #2d3e50;
+    font-family: "Bakbak One";
+    text-align: center;
+    margin: auto;
+    ${Media.Desktop} {
+      font-size: 25px;
+    }
+    ${Media.MobileL} {
+      font-size: 20px;
+    }
+  }
+
+  p {
+    font-size: 30px;
+    color: #2d3e50;
+    font-family: "lato";
+    width: 50%;
+    margin: 0 auto;
+    margin-top: 20px;
+    text-align: center;
+    ${Media.Desktop} {
+      font-size: 20px;
+    }
+    ${Media.MobileL} {
+      font-size: 15px;
+    }
   }
 `;
