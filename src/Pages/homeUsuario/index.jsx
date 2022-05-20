2c3814b4b2f551f80849f441c7d132b7fa71e368
ihyperbr <ihyperbr@gmail.com>
2022-05-20 11:19:21 -0300

    home User

--- src/Pages/homeUsuario/index.jsx
+++ src/Pages/homeUsuario/index.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import Carrinho from "./images/carrinho.png";
-import Shopping from "./images/shopping.png";
-import Mercado from "./images/mercado.png";
-import Farmarcia from "./images/farmarcia.png";
-import Restaurante from "./images/restaurantes.png";
 import { Header } from "../../Components/HeaderUsuario/HeaderUser";
 import { FeedUser } from "../../Components/Feed/index";
 import { MenuPrincipal } from "../../Components/MenuPrincipal/index";
@@ -25,43 +21,6 @@ export function IndexHomeUser() {
 
         <img src={Carrinho} alt="Carrinho"/>
       </HeaderA>
-
-      <Categoria>
-        <h1 className="aaaaa">CATEGORIAS</h1>
-        <div className="containerCategoria">
-          <CategoriaShopping> 
-            <img src={Shopping} alt="Icone Shopping" />
-            <InfoCategoria>
-              <h2>Shopping</h2>
-              <p> &#x2192; </p>
-            </InfoCategoria>
-          </CategoriaShopping>
-
-          <CategoriaShopping>
-            <img src={Mercado} alt="Icone Shopping" />
-            <InfoCategoria>
-              <h2>Mercado</h2>
-              <p> &#x2192; </p>
-            </InfoCategoria>
-          </CategoriaShopping >
-
-          <CategoriaShopping>
-            <img src={Farmarcia} alt="Icone Shopping" />
-            <InfoCategoria>
-              <h2>Farmárcia</h2>
-              <p> &#x2192; </p>
-            </InfoCategoria>
-          </CategoriaShopping>
-
-          <CategoriaShopping>
-            <img src={Restaurante} alt="Icone Shopping" />
-            <InfoCategoria>
-              <h2>Restaurante</h2>
-              <p> &#x2192; </p>
-            </InfoCategoria>
-          </CategoriaShopping>
-        </div>
-      </Categoria>
 
       <FeedUser />
       <FeedUser />
