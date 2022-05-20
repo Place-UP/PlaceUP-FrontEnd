9d74c48611d8b019835d0b53e955a71fc5903a47
ihyperbr <ihyperbr@gmail.com>
2022-05-20 13:32:11 -0300

    arrumando página reservas

--- src/Pages/reservas/index.jsx
+++ src/Pages/reservas/index.jsx
@@ -1,15 +1,8 @@
 import React from "react";
-import Carrinho from "./images/carrinho.png";
-import Shopping from "./images/shopping.png";
-import Mercado from "./images/mercado.png";
-import Farmarcia from "./images/farmarcia.png";
-import Restaurante from "./images/restaurantes.png";
 import { Header } from "../../Components/HeaderUsuario/HeaderUser";
 import { FeedUser } from "../../Components/Feed/index";
 import { MenuPrincipal } from "../../Components/MenuPrincipal/index";
-import { IoIosArrowDown } from "react-icons/io";
-import { AiOutlineSearch } from "react-icons/ai"
-import { Home, HeaderA, Barra, Categoria, CategoriaShopping, InfoCategoria, Car } from "./styles";
+import { Home, Categoria } from "./styles";
 
 export function Reservas() {
   return (
@@ -19,51 +12,7 @@ export function Reservas() {
         <MenuPrincipal />
 
         <Categoria>
-          <HeaderA>
-            <Barra>
-              <span><AiOutlineSearch size="25px" /></span>
-              <input type="text" className="pesquisa" placeholder="Pesquisa"></input>
-              <button><IoIosArrowDown className="seta" size="25px" /></button>
-            </Barra>
-            <Car>
-              <img src={Carrinho} alt="Carrinho" />
-            </Car>
-          </HeaderA>
-
           <h1 className="aaaaa">MINHAS RESERVAS</h1>
-          <div className="containerCategoria">
-            <CategoriaShopping>
-              <img src={Shopping} alt="Icone Shopping" />
-              <InfoCategoria>
-                <h2>Shopping</h2>
-                <p> &#x2192; </p>
-              </InfoCategoria>
-            </CategoriaShopping>
-
-            <CategoriaShopping>
-              <img src={Mercado} alt="Icone Shopping" />
-              <InfoCategoria>
-                <h2>Mercado</h2>
-                <p> &#x2192; </p>
-              </InfoCategoria>
-            </CategoriaShopping >
-
-            <CategoriaShopping>
-              <img src={Farmarcia} alt="Icone Shopping" />
-              <InfoCategoria>
-                <h2>Farmárcia</h2>
-                <p> &#x2192; </p>
-              </InfoCategoria>
-            </CategoriaShopping>
-
-            <CategoriaShopping>
-              <img src={Restaurante} alt="Icone Shopping" />
-              <InfoCategoria>
-                <h2>Restaurante</h2>
-                <p> &#x2192; </p>
-              </InfoCategoria>
-            </CategoriaShopping>
-          </div>
         </Categoria>
 
         <FeedUser />
--- src/Pages/reservas/styles.js
+++ src/Pages/reservas/styles.js
@@ -14,54 +14,6 @@ export const Home = styled.div`
   }
  `;
 
-export const HeaderA = styled.div`
-    width: 100%;
-    height: 100px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    border-radius: 50%;
-
-    img{
-        width: 30px;
-        height: 30px;        
-        margin: 33px 0px 0px 100px;
-        cursor: pointer;
-    }
-`;
-
-export const Barra = styled.div`
-  width: 100%;
-  margin: auto;
-  display: flex;
-  align-items: center;
-  padding: 0px 2%;
-  justify-content: space-between;
-  background-color: #fff;
-  border-radius: 20px;
-    
-  .pesquisa{
-    width: 95%;
-    height: 40px;
-    padding: 0px 20px 0px 10px;
-    background-color: white;
-    background-size: 15px;
-    background-position: 10px 9px;
-    background-repeat:no-repeat;     
-    border:0;
-    outline: 0;
-  }
-
-  .seta{
-    background-color: #fff;
-  }
-
-  button{ cursor: pointer; }
-`;
-
-export const Car = styled.div`
-
-`
 
 export const Categoria = styled.div`
   max-width: 1400px;
@@ -70,72 +22,7 @@ export const Categoria = styled.div`
 
   h1{   
     font-size: 20px; 
-    margin-left: 5px;
+    padding: 20px 5px;
     font-weight: 800;
   }
-
-  .containerCategoria{
-    width: 100%;
-    display: grid;
-    grid-template-columns: auto auto auto auto;
-    flex-direction: row;
-    justify-content: space-between;
-    ${media.Tablet}{
-      width: 120%;
-      grid-template-columns: auto auto auto auto;
-      grid: auto / auto auto;
-    }
-
-    ${media.MobileS}{
-      width: 50%;
-      margin-left: 20%;
-      grid-template-columns: auto auto auto 20px;
-      grid: auto / auto;
-    }
-  }
-
-  ${media.MobileS}{
-    h1{   margin-left: 24px;    }
-  }
-`;
-
-export const CategoriaShopping = styled.div`
-    width: 120px;
-    height: 100px;
-    border-radius: 20px;
-    margin-top: 70px;
-    text-align: center; 
-    background-color: white;
-
-    img{
-        width: 110px;
-        height: 110px;
-        font-size: 17px;
-        margin-top: -50px;
-    }
-`;
-
-export const InfoCategoria = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;    
-    border-left: 2px solid #E85959;
-
-    h2{   
-        font-size: 15px; 
-        margin-left: 5px;
-        font-weight: 600;
-        cursor: pointer;
-    }
-
-    p{
-        width: 20px;
-        height: 20px;
-        background-color: #E85959;
-        color: white;
-        font-size: 17px;
-        border-radius: 10px;
-        line-height: -200px;
-        cursor: pointer;
-    }
 `;
