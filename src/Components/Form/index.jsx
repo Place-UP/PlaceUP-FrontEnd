9735e16cf9a9030992318cc0fa64d1c16f4122f1
Gustavo <gustavopereirafacal@gmail.com>
2022-06-01 18:24:21 -0300

    Validação Form

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,27 +1,71 @@
-
+import React, { useState } from 'react'
 //---------------------Componentes---------------------------
 import { CheckBox } from "../../Components/CheckBox";
 import { Form, Label, Remember, Submit } from "./style";
-
 
 //----------------Icones---------------------
 import { HiUser } from "react-icons/hi";
 import { MdEmail } from "react-icons/md";
 
+function useFormik({
+    initialValues
+}) {
+
+    const [values, setValues] = useState(initialValues)
+
+    function handleChange(event) {
+        const fieldname = event.target.getAttribute('name')
+        const value = event.target.value
+        setValues({
+            ...values,
+            [fieldname]: value,
+        })
+    }
+
+    return {
+        values,
+        handleChange
+    }
+}
+
 export const Forms = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: ''
+        }
+    })
+
     return (
         <>
-            <Form>
+            <Form onSubmit={(e) => {
+                e.preventDefault()
+                console.log(formik.values)
+
+            }}>
                 <legend>Cadastro</legend>
 
                 <Label htmlFor="name">
                     <i><HiUser /></i>
-                    <input type="text" name="name" id="name" placeholder="Nome" required />
+                    <input type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Nome"
+                        onChange={formik.handleChange}
+                        value={formik.values.name}
+                        required />
                 </Label>
 
                 <Label htmlFor="email">
                     <i><MdEmail /></i>
-                    <input type="email" name="email" id="email" placeholder="E-mail" required />
+                    <input type="email"
+                        name="email"
+                        id="email"
+                        placeholder="E-mail"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                        required />
                 </Label>
 
                 <Remember>
